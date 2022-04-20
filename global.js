87b5b44b53379cc1548757098853f83facaf30d8
hadika13522 <100363859+hadika13522@users.noreply.github.com>
2022-04-20 16:15:12 +0600

    Add files via upload

---
+++ global.js
@@ -0,0 +1,16 @@
+// //global object
+// // console.log(global);
+// setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(int);
+// },3000);
+
+// const int=setInterval(()=>{
+//     console.log('in the interval');
+// },1000)
+
+// console.log(__dirname);//gets ansolute path of the current folder that global file is in
+// console.log(__filename);//gets absolute path of the folder with the filename
+
+
+console.log(document.querySelector);
